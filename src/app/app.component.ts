55755f5872304f39ca5759b4fce649ae0c9de970
seanjkr <seankoppreddy@gmail.com>
2020-11-21 19:36:26 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { QuestionsService } from './questions.service';
+import { Quiz , Answers , Choice } from './quiz.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector : 'app-root',
+  templateUrl : './app.component.html',
+  styleUrls : ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ultimate-quiz-app';
+  answers : Answers;
+  quiz : Quiz;
+  currentQuestionIndex : number;
+  private showResults = false;
+
+  constructor( private questionsService : QuestionsService ) {
+    this.questionsService.getJSON( 'radiohead' ).subscribe( data => {
+
+      this.quiz = new Quiz( 'radiohead' , data );
+      this.answers = new Answers();
+      this.currentQuestionIndex = 0;
+    });
+  }
+
+  updateChoice( choice: Choice ) {
+    this.answers.values[ this.currentQuestionIndex ] = choice;
+  }
+
+  nextOrViewResults() {
+    if ( this.currentQuestionIndex === this.quiz.questions.length - 1 ) {
+      this.showResults = true;
+      return;
+    }
+
+    this.currentQuestionIndex++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations : [
+    AppComponent,
+    QuestionFormComponent
   ],
-  imports: [
-    BrowserModule
+
+  imports : [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers : [],
+
+  bootstrap : [ AppComponent ]
+
 })
-export class AppModule { }
+
+export class AppModule {}
